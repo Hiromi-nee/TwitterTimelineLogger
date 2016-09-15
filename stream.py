97d2af995c1f9c2359f1ebb572364b22a209e032
Hiromi-nee <kyou.wk@gmail.com>
2016-09-15 23:13:05 +0800

    logging

--- stream.py
+++ stream.py
@@ -4,13 +4,16 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import json
 import requests
-import csv
+import re
+import logging
 
 #Variables that contains the user credentials to access Twitter API
 #creds file, 0=access_token,1=access_token_secret,2=consumer_key,3=consumer_secret
 creds_file = "creds.txt"
 lines = [line.rstrip('\n') for line in open('id.txt')]
 jsonout = open("out.json",'w')
+rt_re = re.compile('^RT')
+
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
@@ -25,23 +28,24 @@ class StdOutListener(StreamListener):
                 sdata['name'] = p_json['user']['name']
                 sdata['username'] = p_json['user']['screen_name']
                 sdata['qt'] = p_json['is_quote_status']
-                sdata['rt'] = p_json['retweeted']
+                #retweeted field always false, so to detect retweets, use regex
+                if rt_re.match(p_json['text']):
+                    sdata['rt'] = True
                 sdata['text'] = p_json['text']
                 sdata['coordinates'] = p_json['coordinates']
                 sdata['source'] = p_json['source']
                 json.dump(p_json, jsonout, indent=4, sort_keys=True)
                 print( sdata )
         except Exception as some_exception:
-            pass
+            logging.debug(some_exception)
         return True
 
     def on_error(self, status):
-        #print status
-        pass
+        logging.debug("Twitter error: "+status)
 
 if __name__ == '__main__':
-    creds = [line.rstrip('\n') for line in open(creds_file)]
-        
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    creds = [line.rstrip('\n') for line in open(creds_file)]    
     l = StdOutListener()
     #auth = OAuthHandler(consumer_key, consumer_secret)
     #auth.set_access_token(access_token, access_token_secret)
@@ -50,8 +54,8 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
     try:
         stream.filter(follow=lines)
-    except AttributeError:
-        pass
+    except AttributeError as a_error:
+        logging.debug(a_error)
 
 
 
