f7a7283a972e7264a8f7aa7708eaf01d437cb054
Hiromi-nee <kyou.wk@gmail.com>
2016-09-19 20:37:29 +0800

    twitterid.py

--- tools/twitterid.py
+++ tools/twitterid.py
@@ -3,20 +3,17 @@ from tweepy import OAuthHandler
 from tweepy import API
 import sys
 
-
 def main(screen_name):
-	"""
-	Gets twitter id for given screen name
-	Usage: python twitterid.py screen_name
-	"""
-    auth = OAuthHandler("consumer_key",
-                        "consumer_secret")
-    api = API(auth)
-    user_details = api.get_user(screen_name)
-    user_id = user_details.id
-    print(user_id)
+        """
+        Gets twitter id of given screen name
+        Usage: python twitterid.py screen_name
+        """
+        auth = OAuthHandler("consumer key", "consumer secret")
+        api = API(auth)
+        user_details = api.get_user(screen_name)
+        user_id = user_details.id
+        print(user_id)
 
 if __name__ == '__main__':
-    screen_name = sys.argv[1]
-
-    main(screen_name)
+        screen_name = sys.argv[1]
+        main(screen_name)
